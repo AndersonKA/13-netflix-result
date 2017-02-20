f9025c931706ad5c36110a9abae286e0813d67e4
Kae Anderson <andersonkaem@gmail.com>
2017-02-20 16:48:41 -0600

    started JS

---
+++ app/fill-in-results.js
@@ -0,0 +1,11 @@
+export default {
+  fillInResult(element, data) {
+
+  }
+};
+
+// Create a module fill-in-result.js that exports a function
+// accepts two arguments
+// element to fill in
+// data object (see example response data below)
+// Fill in the elements within the element argument using the data from the argument (see below for an example response)
