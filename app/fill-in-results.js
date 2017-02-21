00cf9bf6bbea766e5101ce1435cf764dabdfcfd1
Kae Anderson <andersonkaem@gmail.com>
2017-02-21 09:01:22 -0600

    worked on JS

--- app/fill-in-results.js
+++ app/fill-in-results.js
@@ -1,7 +1,6 @@
-export default {
-};
-// Create a module fill-in-result.js that exports a function
-// accepts two arguments
-// element to fill in
-// data object (see example response data below)
-// Fill in the elements within the element argument using the data from the argument (see below for an example response)
+export default function fillInResult(element, data) {
+    element.querySelector('.upper-title').innerHTML = data.show_title;
+}
+
+// Fill in the elements within the element argument using the data
+// from the argument (see below for an example response)
