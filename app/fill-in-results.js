1ebce0ccbd44dfa648eb25588d6387c7a4b217e1
Kae Anderson <andersonkaem@gmail.com>
2017-02-21 14:10:41 -0600

    Kate helped me with the JS

--- app/fill-in-results.js
+++ app/fill-in-results.js
@@ -1,6 +1,8 @@
 export default function fillInResult(element, data) {
-    element.querySelector('.upper-title').innerHTML = data.show_title;
+  element.querySelector('.upper-title').innerText = data.show_title;
+  element.querySelector('.upper-category').innerText = data.category;
+  element.querySelector('.upper-rating').innerText = data.rating;
+  element.querySelector('.upper-pic').src = data.poster;
+  element.querySelector('.lower-summary__words').innerText = data.summary;
+  element.querySelector('.lower-actors__words').innerText = data.show_cast;
 }
-
-// Fill in the elements within the element argument using the data
-// from the argument (see below for an example response)
--- app/index.js
+++ app/index.js
@@ -1,33 +1,20 @@
 import 'whatwg-fetch';
-import stuff from './fill-in-results';
+import fillInResult from './fill-in-results';
 
 // * [ ] Create Variables for the search form, search input, and search result
 const searchForm = document.querySelector('.search-button');
 const searchInput = document.querySelector('.search-field');
-const searchResult = document.querySelector('.upper');
+const searchResult = document.querySelector('.main');
 
-function fillInMovie(name) {
-  fetch(`http://netflixroulette.net/api/api.php?title=Attack%20on%20titan${name}`).then((res) => {
-    return res.json();
-  }).then((data) => {
-    fillInResult(data.results);
+function fillInMovie(test) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${test}`)
+  .then(response => response.json())
+  .then((data) => {
+    fillInResult(searchResult, data);
   });
 }
-
-
 searchForm.addEventListener('click', () => {
   fillInMovie(searchInput.value);
 });
 
-// * [ ] Make a new fetch request when the page is submitted replacing `
-// Attack%20on%20titan` with the input value from your form
-// * [ ] When the fetch finishes
-// * [ ] Use a `then` promise chain to unravel the JSON response from the API
-// * [ ] In the `then` callback:
-// * [ ] run the `fillInResult` function passing in the wrapping element and the
-// resulting object from the fetch request
-//
-//
-//
-// For this one you won't need to `createElement` in JS. Just query selector
-// it up and change contents based on the data you get from the API;
+fillInMovie('Attack on Titan');
