c84561d965c6610fe2056d4765f5af104e03e389
Kae Anderson <andersonkaem@gmail.com>
2017-02-21 14:48:26 -0600

    fixed centering issue

--- app/index.js
+++ app/index.js
@@ -6,15 +6,17 @@ const searchForm = document.querySelector('.search-button');
 const searchInput = document.querySelector('.search-field');
 const searchResult = document.querySelector('.main');
 
+// make a new function with a new argument
 function fillInMovie(test) {
+  // look for the API and add the argument to the end
   fetch(`http://netflixroulette.net/api/api.php?title=${test}`)
+
   .then(response => response.json())
   .then((data) => {
     fillInResult(searchResult, data);
   });
 }
+// when the search button is clicked on
 searchForm.addEventListener('click', () => {
   fillInMovie(searchInput.value);
 });
-
-fillInMovie('Attack on Titan');
